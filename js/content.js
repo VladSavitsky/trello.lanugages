040a22d4f4e4722b9b3f9fac3405ed8d8afea653
Vlad Savitsky <vlad.savitsky@gmail.com>
2015-05-10 05:42:49 +0300

    Fixed translations.

--- js/content.js
+++ js/content.js
@@ -14,88 +14,18 @@
 */
 
 (function($) {
-
-  // Load mapping data.
-  var flags = $.parseJSON(getFile(chrome.extension.getURL('/flags.json')));
+  var flags = getFile(chrome.extension.getURL('/flags.json'));
+  if (typeof flags == 'string') {
+    flags = $.parseJSON(flags);
+  }
   $.each(flags, function(context, cssPath) {
     if (cssPath != "") {
       $(cssPath + ':not(.translated)').waitUntilExists(function() {
         l10n(context);
-      }).addClass('translated');
+        $(this).addClass('translated');
+      });
     }
   });
-
-  // Replace strings at page using mapping from translation object.
-  function l10n(context) {
-    var mapping = $.parseJSON(getMapping(context));
-    $.each(mapping, function(type, data) {
-      if (type == 'meta') return;
-      $.each(data, function(code, selectors) {
-//        if (!chrome.i18n.getMessage(code)) return;
-        if ($.type(selectors) === 'string') selectors = selectors.split();
-        $.each(selectors, function(index, cssPath) {
-          translateElement(type, code, mapping["meta"]['basePath'] + cssPath);
-        });
-      });
-    });
-  };
-
-  // Translate element by given cssPath.
-  function translateElement(type, code, cssPath) {
-    var $element = getElement(cssPath);
-    if (!$element.length) return;
-    // Store original string.
-    $element.prop('data-translation-code-' + type, code);
-
-    // Do the magic: replace original string with translation.
-    if (type == 'title' || type == 'placeholder') {
-      $.each($element, function() {
-        $(this).attr(type, function(index, el) {
-          if (cssPath == "#permission-level" && type == 'title') {
-            if (escapeString($(this).attr(type).trim()) == code) {
-              // We must to do that extra check because some elements
-              // has the same CSS Path but different content text.
-              return el.replace($(this).attr(type).trim(), chrome.i18n.getMessage(code));
-            }
-          }
-          else {
-            return el.replace($(this).attr(type).trim(), chrome.i18n.getMessage(code));
-          }
-        });
-      });
-    } else if (type == 'html') {
-      $.each($element, function() {
-        $(this).html(function(index, el) {
-          return el.replace($(this).text().trim(), chrome.i18n.getMessage(code));
-        });
-      });
-    } else if (type == 'inputs') {
-      $.each($element, function() {
-        $element.val(chrome.i18n.getMessage(code));
-      });
-    } else if (type == 'substrings') {
-      // First we need to check if there is a span tag with original string.
-      // If tag exists we simply replace it's content with new translation.
-      var $tag = $element.find('.language-source-text[data-original="' + code + '"]');
-      if ($tag.length) {
-        // Tag exists. Just translate text in SPAN.
-        $.each($tag, function() {$(this).html(' ' + chrome.i18n.getMessage(code) + ' ')});
-      }
-      else {
-        // There is no tag and we should wrap substring with SPAN tag to store original sring.
-        // This should be done to allow translations to another languages.
-        $tag = $('<span />').addClass('language-source-text').attr('data-original', code).html(chrome.i18n.getMessage(code));
-        // Replace substring with special tag.
-        $element.html(function(index, html) {
-          // We should add trailing spaces to avoid translations in card name or etc.
-          // trello.com uses extra spaces for words which are common but this could be changed anytime
-          // and we shouldn't rely on this.
-          // TODO: implement translations of phrases with placeholders. Eg., '%name added %cardname'.
-          return html.replace(' ' + code + ' ', ' ' + $tag[0].outerHTML + ' ');
-        });
-      }
-    }
-  }
 
 /*
 
--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,9 @@ var debug = chrome.extension.getURL('/manifest.json').indexOf('alhoallabckfhacph
 
 /**
  * Synchronously load file and return it's content.
+ *
  * @var string File URL.
+ *
  * @return string Returns file content as a string.
  * @see http://forum.jquery.com/topic/how-do-i-access-json-data-outside-of-getjson
  */
@@ -27,7 +29,8 @@ var getFile = (function getFile(url) {
     }
   });
   return result;
-}).memoize();
+/* }).memoize();*/
+});
 
 /**
  * Check if JSON file was loaded and given object not empty.
@@ -41,7 +44,7 @@ function getMapping(context) {
   var mapping = getFile(chrome.extension.getURL(filename));
   if (jQuery.isEmptyObject(mapping)) {
     if (debug) {
-      console.log(filename + ' is missing or broken.')
+      console.error(filename + ' is missing or broken.')
     };
     return false;
   }
@@ -61,13 +64,107 @@ function escapeString(string) {
 }
 
 
-function getElement(cssPath) {
+function getElement(type, code, cssPath, context) {
   var $element = $(cssPath);
   if (!$element.length) {
-    if (debug) {
-      console.log(cssPath, "\n", 'Message: element exists in [mapping].json but wasn\'t found at page.');
-    }
+    buildMessage(type, code, cssPath, context, "Element wasn't found at page.");
     return false;
   }
   return $element;
+}
+
+function buildMessage(type, code, cssPath, context, message) {
+  if (debug) {
+    var logMessage = {
+        "Message" : {"value" : message},
+        "Context" : {"value" : context},
+        "Type" : {"value" : type},
+        "Code" : {"value" : code},
+        "Translation" : {"value" : chrome.i18n.getMessage(code)},
+        "cssPath" : {"value" : cssPath}
+    };
+    console.table(logMessage);
+  }
+}
+
+// Replace strings at page using mapping from translation object.
+function l10n(context) {
+  var mapping = getMapping(context);
+  if (typeof mapping == 'string') {
+    mapping = $.parseJSON(mapping);
+  }
+  $.each(mapping, function(type, data) {
+    if (type == 'meta') return;
+    $.each(data, function(code, selectors) {
+//        if (!chrome.i18n.getMessage(code)) return;
+      if ($.type(selectors) === 'string') selectors = selectors.split();
+      $.each(selectors, function(index, cssPath) {
+        translateElement(type, code, mapping["meta"]['basePath'] + ' ' + cssPath, context);
+      });
+    });
+  });
+};
+
+// Translate element by given cssPath.
+function translateElement(type, code, cssPath, context) {
+  var $element = getElement(type, code, cssPath, context);
+  if (!$element.length) {
+    buildMessage(type, code, cssPath, context, "Element was NOT found.");
+    return;
+  }
+  if (!chrome.i18n.getMessage(code)) {
+    buildMessage(type, code, cssPath, context, "No translation found for this element.");
+    return;
+  }
+  // Store original string.
+  $element.prop('data-translation-code-' + type, code);
+
+  // Do the magic: replace original string with translation.
+  if (type == 'title' || type == 'placeholder') {
+    $.each($element, function() {
+      $(this).attr(type, function(index, el) {
+        if (cssPath == "#permission-level" && type == 'title') {
+          if (escapeString($(this).attr(type).trim()) == code) {
+            // We must to do that extra check because some elements
+            // has the same CSS Path but different content text.
+            return el.replace($(this).attr(type).trim(), chrome.i18n.getMessage(code));
+          }
+        }
+        else {
+          return el.replace($(this).attr(type).trim(), chrome.i18n.getMessage(code));
+        }
+      });
+    });
+  } else if (type == 'html') {
+    $.each($element, function() {
+      $(this).html(function(index, el) {
+        return el.replace($(this).text().trim(), chrome.i18n.getMessage(code));
+      });
+    });
+  } else if (type == 'inputs') {
+    $.each($element, function() {
+      $element.val(chrome.i18n.getMessage(code));
+    });
+  } else if (type == 'substrings') {
+    // First we need to check if there is a span tag with original string.
+    // If tag exists we simply replace it's content with new translation.
+    var $tag = $element.find('.language-source-text[data-original="' + code + '"]');
+    if ($tag.length) {
+      // Tag exists. Just translate text in SPAN.
+      $.each($tag, function() {$(this).html(' ' + chrome.i18n.getMessage(code) + ' ')});
+    }
+    else {
+      // There is no tag and we should wrap substring with SPAN tag to store original sring.
+      // This should be done to allow translations to another languages.
+      $tag = $('<span />').addClass('language-source-text').attr('data-original', code).html(chrome.i18n.getMessage(code));
+      // Replace substring with special tag.
+      $element.html(function(index, html) {
+        // We should add trailing spaces to avoid translations in card name or etc.
+        // trello.com uses extra spaces for words which are common but this could be changed anytime
+        // and we shouldn't rely on this.
+        // TODO: implement translations of phrases with placeholders. Eg., '%name added %cardname'.
+        return html.replace(' ' + code + ' ', ' ' + $tag[0].outerHTML + ' ');
+      });
+    }
+  }
 }
